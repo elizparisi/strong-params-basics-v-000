be279c83affe2eeb78024961251f83dd3ace0a1f
Liz P <parisielizabeth@gmail.com>
2019-11-10 22:25:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,14 +22,14 @@ class PostsController < ApplicationController
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
+	
 	def edit
 	  @post = Post.find(params[:id])
 	end
+	
 	private
  
   def post_params(*args)
     params.require(:post).permit(*args)
   end
-
-	
 end
